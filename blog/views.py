d125603279ac23e794c150015ef5373eb2025b4c
ahmetbulut <ahmetbulut@gmail.com>
2023-05-18 12:42:48 +0300

    Form example for expressing user queries (retrieving objects that match user criteria) via GET requests. The contents of a sample deployment script.

--- blog/views.py
+++ blog/views.py
@@ -7,7 +7,6 @@ def blog_view(request):
     # query your data store and get the objects you need.
     #books = ["Skin in the Game", "Antifragile", "Da Vinci's Code", "Blink", "Tesla"]
 
-    books = Book.objects.all()
-
-    return render(request, "blog_post.html", context = {"user": request.META['USER'],
+    books = Book.objects.filter(title__startswith=request.GET['filter'])
+    return render(request, "blog_home.html", context = {"user": request.META['USER'],
                                                         "books": books})
