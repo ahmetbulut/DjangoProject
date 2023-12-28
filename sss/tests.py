60028d13fc1a2f590c134773a1c5d1aea3a940f3
ahmetbulut@gmail.com <ahmetbulut@gmail.com>
2023-12-28 18:34:12 +0300

    Transaction Demo.

--- sss/tests.py
+++ sss/tests.py
@@ -1,3 +1,44 @@
-from django.test import TestCase
+from unittest import TestCase
+from sss.models import Customer, Address
+from django.db import transaction
 
-# Create your tests here.
+# Run unit tests via cmd line.
+# $ python manage.py test
+
+class MyTests(TestCase):
+
+    def test_X(self):
+        a = Address(street="Customer X Address", state='DC', city='Washington', postcode=20008)
+        a.save() # Write 1
+
+        c = Customer(name="Customer X")
+        c.save() # Write 2
+
+
+    def test_Y(self):
+        a = Address(street="Customer Y Address", state='DC', city='Washington', postcode=20008)
+        a.save() # Write 1
+
+        raise Exception # things could go wrong right in the middle!
+
+        c = Customer(name="Customer Y")
+        c.save() # Write 2
+
+    @transaction.atomic
+    def test_txn_Z(self):
+        a = Address(street="Customer Z Address", state='DC', city='Washington', postcode=20008)
+        a.save() # Write 1
+
+        c = Customer(name="Customer Z")
+        c.save() # Write 2
+
+
+    @transaction.atomic
+    def test_txn_W(self):
+        a = Address(street="Customer W", state='DC', city='Washington', postcode=20008)
+        a.save()  # Write 1
+
+        raise Exception  # things could go wrong right in the middle!
+
+        c = Customer(name="Customer W")
+        c.save()  # Write 2
